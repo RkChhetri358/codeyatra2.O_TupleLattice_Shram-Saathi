e559c29461abdeb96ed4cf63443218ba686370ee
Roshanbist48 <roshanbist48@gmail.com>
2026-02-22 17:50:06 +0545

    navbar ongoing

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,12 +2,12 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-d
 import LandingPage from "./components/LandingPage/LandingPage";
 import Login from "./Components/Loginpage/Loginpage";
 import Signup from "./components/Signup/Signup";
+import Home from "./components/Home/Home"; // Home lai uncomment gareko
 
 // Baki components haru paxi chaine bela uncomment garna sakincha
 // import Dashboard from "./components/Dashboard/Dashboard";
 // import Profile from "./components/Profile/Profile";
 // import Layout from "./components/Layout/Layout";
-// import Home from "./components/Home/Home";
 // import AddAsset from "./Components/AddAsset/AddAsset";
 // import Asset from "./Components/Asset/Asset";
 // import SellingAsset from "./Components/SellingAsset/SellingAsset";
@@ -24,9 +24,11 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
 
+        {/* Home Page Route - Login garesi yetai redirect huncha */}
+        <Route path="/home" element={<Home />} />
+
         {/* Dashboard Layout and Protected Routes (Paxi use garna lai ready rakheko) */}
-        {/* <Route path="/home" element={<Home />} />
-        <Route path="/addasset" element={<AddAsset />} />
+        {/* <Route path="/addasset" element={<AddAsset />} />
         <Route path="/layout" element={<Layout />}>
           <Route index element={<Dashboard />} />            
           <Route path="dashboard" element={<Dashboard />} /> 
