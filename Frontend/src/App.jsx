763aa8e87a7b3bd46a6c260e79135521d4596efd
Roshanbist48 <roshanbist48@gmail.com>
2026-02-22 21:11:45 +0545

    consumer_home is done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,45 +1,36 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import LandingPage from "./components/LandingPage/LandingPage";
+import LandingPage from "./Components/LandingPage/LandingPage";
 import Login from "./Components/Loginpage/Loginpage";
 import Signup from "./Components/Signup/Signup";
-import Home from "./Components/Home/Home"; // Home lai uncomment gareko
+import Home from "./Components/Home/Home";
+// 1. Exact Folder ra File name ConsumerHome nai hunu parchha
+import ConsumerHome from "./Components/ConsumerHome/ConsumerHome"; 
 
-// Baki components haru paxi chaine bela uncomment garna sakincha
-// import Dashboard from "./components/Dashboard/Dashboard";
-// import Profile from "./components/Profile/Profile";
-// import Layout from "./components/Layout/Layout";
-// import AddAsset from "./Components/AddAsset/AddAsset";
-// import Asset from "./Components/Asset/Asset";
-// import SellingAsset from "./Components/SellingAsset/SellingAsset";
-// import BuyingAsset from "./Components/BuyingAsset/BuyingAsset";
+// Baki components commented chhan
+// import Dashboard from "./Components/Dashboard/Dashboard";
+// import Profile from "./Components/Profile/Profile";
+// import Layout from "./Components/Layout/Layout";
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Landing Page Route */}
         <Route path="/" element={<LandingPage />} />
-        
-        {/* Auth Routes */}
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-
-        {/* Home Page Route - Login garesi yetai redirect huncha */}
         <Route path="/home" element={<Home />} />
 
-        {/* Dashboard Layout and Protected Routes (Paxi use garna lai ready rakheko) */}
-        {/* <Route path="/addasset" element={<AddAsset />} />
-        <Route path="/layout" element={<Layout />}>
+        {/* 2. Aba URL ma "http://localhost:5184/ConsumerHome" handa yo khulchha */}
+        <Route path="/ConsumerHome" element={<ConsumerHome />} />
+
+        {/* Dashboard Layout Routes (Ready for future) */}
+        {/* <Route path="/layout" element={<Layout />}>
           <Route index element={<Dashboard />} />            
           <Route path="dashboard" element={<Dashboard />} /> 
-          <Route path="asset" element={<Asset/>} />
-          <Route path="sellingasset" element={ <SellingAsset/>} />
-          <Route path="buyingasset" element={ <BuyingAsset/>} />
           <Route path="profile" element={<Profile />} />
         </Route> 
         */}
 
-        {/* Catch-all route: redirects to login if path doesn't match */}
         <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
     </Router>
